3408332216db9b380bf3e956cf6007641a6fcdce
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-25 22:59:21 -0600

    Amends show article spec for comments

--- spec/features/user_sees_single_article_spec.rb
+++ spec/features/user_sees_single_article_spec.rb
@@ -1,17 +1,22 @@
 require "rails_helper"
 
 describe "user sees one article" do
-	describe "they link from the articles index" do
-		it "displays information for one article" do
+  describe "they link from the article index" do
+    it "displays information for one article" do
+      article = Article.create!(title: "New Title", body: "New Body")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
-			article = Article.create!(title: "My title", body: "My body")
-			visit articles_path
+      visit articles_path
 
-			click_link article.title
+      click_link article.title
 
-			expect(page).to have_content(article.title)
-			expect(page).to have_content(article.body)
-
-		end
-	end
+      expect(page).to have_content(article.title)
+      expect(page).to have_content(article.body)
+      expect(page).to have_content(comment_1.author_name)
+      expect(page).to have_content(comment_1.body)
+      expect(page).to have_content(comment_2.author_name)
+      expect(page).to have_content(comment_2.body)
+    end
+  end
 end
