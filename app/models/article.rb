07f9054f9fae1c8a634584f441e91aabf7d50624
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-25 23:41:59 -0600

    Adds has_many, through relationships for tag and article

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,5 @@ class Article < ApplicationRecord
   validates_presence_of :title, :body
   has_many :comments
   has_many :taggings
+  has_many :tags, through: :taggings
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,5 @@
 class Tag < ApplicationRecord
   validates_presence_of :name
   has_many :taggings
+  has_many :articles, through: :taggings
 end
