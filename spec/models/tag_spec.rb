453b36f538263a8c566a521f1f8e829fb074a362
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-03-25 23:59:42 -0600

    Fixes strong params to allow for tags, adds instance method spec tests for article and tag, adds the methods to tag, adds html to accomodate

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -8,4 +8,14 @@ describe Tag, type: :model do
   describe "relationships" do
     it {should have_many(:taggings)}
   end
+
+  describe "instance methods" do
+    describe "#to_s" do
+      it "returns string of tag name" do
+        tag = Tag.create!(name: 'A tag')
+
+        expect(tag.to_s).to eq(tag.name)
+      end
+    end
+  end
 end
